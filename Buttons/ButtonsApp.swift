296c6b2f9b25081c1d36957df3fe9cf027336d5a
Miguel de Icaza <miguel@gnome.org>
2021-06-18 22:21:01 -0400

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Miguel de Icaza on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buttons
+//
+//  Created by Miguel de Icaza on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
